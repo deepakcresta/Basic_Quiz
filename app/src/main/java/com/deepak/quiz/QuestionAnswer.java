f8dc5c8e3cddfd597384e735699a97fa54048241
Deepak Shrestha <deepak.shrestha@sbsolutionsnepal.com>
2023-04-02 21:21:33 +0545

    iinitailse commit done

---
+++ app/src/main/java/com/deepak/quiz/QuestionAnswer.java
@@ -0,0 +1,23 @@
+package com.deepak.quiz;
+
+public class QuestionAnswer {
+    public static String question[] ={
+            "Who is the first women minster of Nepal",
+            "Who is known as the green star of Nepal?",
+            "Who is the current Educational Minster of Nepal?",
+    };
+
+    public static String choices[][] = {
+            {"Dwarika Devi Thakuanrni","Onsari Gharti Magar","Bidya Devi Bhandari","Tosima Kaki"},
+            {"sita","Bhirkuti", "Pasang Lamu Sherpa","Geeta"},
+            {"Rbi Lamichnay" ,"Dr. Baburam Bhatta","Shisir Khanal","Krishna Prasad Sitaula"},
+            {"Google","Apple","Nokia","Samsung"}
+    };
+
+    public static String correctAnswers[] = {
+            "Dwarika Devi Thakuanrni",
+            "Bhirkuti",
+            "Shisir Khanal"
+    };
+
+}
